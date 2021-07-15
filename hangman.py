cc9464f521fd1816571e48c58a4322903f7a106f
Andrew Bruce Lau <andrew@zaber.com>
2021-07-14 23:50:12 -0700

    Add function to retrieve word from word file.

--- hangman.py
+++ hangman.py
@@ -1,9 +1,27 @@
 """Test out wordclass."""
 
+import random
+
 from wordclass import Word
 
-word = Word("Apple")
-print(f"Try to guess the word '{word.word}'")
+WORD_FILE = "./data/words.txt"
+
+
+def get_word_from_file() -> str:
+    """Retrieve a random word longer than four characters from word file."""
+    words = open(WORD_FILE, 'r').read().splitlines()
+    word = ''
+    while not word:
+        word = random.choice(words)
+        if len(word) <= 4:
+            word = ''
+    return word
+
+
+word = Word(get_word_from_file())
+# print(f"Try to guess the word '{word.word}'")
+
+print("Let's play Hangman!")
 
 while not word.win():
     print(word)
@@ -13,5 +31,6 @@ while not word.win():
     else:
         print("Try again.")
     print(f"Tried {word.tries} times - Guesses so far: {word.guesses}")
+    print()
 
 print(f"We have a winner: {word}")
